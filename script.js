75939e7b9b5c8b422086f1fda098b3413334b708
Jake Baker <jake.baker.89@hotmail.co.uk>
2020-01-05 10:43:25 +0000

    Continued implementation of the What3Words API. Updated the script.js page to contain all of the relevant JS code for the What3Words API callout: - include actions in the function: - create a new geolocationData variable to hold the JSON output from the What3Words API - use the JSON.parse JS function to convert the JSON data returned to a JS array - store the JS array in the geolocationData variable - use document.getElementByID to retrieve the relevant array object: in this case "words"
    Created a JS_test.html page to test the functionality.
    This uses the HTTP request provided on the What3Words website. This will need to be adapted to include the latitude and longitde variables retrieved from the geolocation callout to get the correct response for the user.

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // declare a new request variable
-var request = new XMLHttpRequest()
+var request = new XMLHttpRequest();
 
 //Connect to the URL of the api using the URL provided on page https://developer.what3words.com/public-api/docs#overview
 //Include API key
 //use the GET method as suggested
-request.open('GET','https://api.what3words.com/v3/convert-to-3wa?coordinates=51.521251%2C-0.203586&key=U4YS2DD5',true)
-
-
+request.open('GET','https://api.what3words.com/v3/convert-to-3wa?coordinates=51.521251%2C-0.203586&key=U4YS2DD5',true);
 
 request.onload = function() {
     //when the request loads, get the data
     //the returned data from the What3Words API is in JSON
+    //create a gelocationData variable to hold the data
+    // the JSON.parse function converts the data to an array of JS objects
+    var geolocationData = JSON.parse(this.response);
 
-}
+    //display the data
+    //return the relevant value of words from the array and print it in the element with the id of "test" in the JS_test page
+    document.getElementById("test").innerHTML = geolocationData.words;
+};
 
 // Send request
-request.send()
+request.send();
+
